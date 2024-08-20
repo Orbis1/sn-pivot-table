8b423099ed99dd91ff256a9ef2852e64e542c3cf
orbis <infectores@gmail.com>
2024-08-20 15:42:57 +0300

    fix: all.json generation

--- src/locale/scripts/generate-all.mjs
+++ src/locale/scripts/generate-all.mjs
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const LOCALES_DIR = path.resolve(dirname, "../locales");
-const LOCALES_FILES = globbySync([`${LOCALES_DIR}/*.json`]);
+const LOCALES_FILES = globbySync([`${LOCALES_DIR}/*.json`.replace(/\\/g, "/")]);
 const LOCALE_PKG_DIR = path.resolve(dirname, "..");
 const ALL = path.resolve(`${LOCALE_PKG_DIR}`, "all.json");
 
